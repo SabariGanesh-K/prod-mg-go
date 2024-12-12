f5ba573e3afa5072d18cc4baedfee5d45b365d5b
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2024-12-13 01:10:28 +0530

    REFACTORING Redis Cache setted up [DONE]

--- api/product.go
+++ api/product.go
@@ -6,8 +6,8 @@ import (
 	"mime/multipart"
 	"net/http"
 
-	"github.com/streadway/amqp"
 	"github.com/rs/zerolog/log"
+	"github.com/streadway/amqp"
 
 	db "github.com/SabariGanesh-K/prod-mgm-go/db/sqlc"
 	"github.com/SabariGanesh-K/prod-mgm-go/s3aws"
@@ -16,26 +16,29 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 )
+
 type CreateProductRequest struct {
-	ID                 string   `form:"id"`
-	UserID             string   `form:"user_id"`
-	ProductName        string   `form:"product_name"`
-	ProductDescription string   `form:"product_description"`
-	ProductPrice       string   `form:"product_price"`
-	File *multipart.FileHeader `form:"file" `
+	ID                 string                `form:"id"`
+	UserID             string                `form:"user_id"`
+	ProductName        string                `form:"product_name"`
+	ProductDescription string                `form:"product_description"`
+	ProductPrice       string                `form:"product_price"`
+	File               *multipart.FileHeader `form:"file" `
 	// ProductUrls        []string `json:"product_urls"`
 }
+
 // add file to s3 codes
 
-func (server *Server) createProduct(ctx *gin.Context){
-	var req  CreateProductRequest
-	if err := ctx.ShouldBind(&req); err != nil { 
-        ctx.JSON(http.StatusBadRequest, errorResponse(err))
-        return
-    }
+func (server *Server) createProduct(ctx *gin.Context) {
+	var req CreateProductRequest
+	if err := ctx.ShouldBind(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-	//add file to s3 
+	//add file to s3
 
 	sess, err := session.NewSessionWithOptions(session.Options{
 		Profile: "default",
@@ -51,34 +54,32 @@ func (server *Server) createProduct(ctx *gin.Context){
 	bucketName := "elasticbeanstalk-us-east-1-686995207617"
 	uploader := s3manager.NewUploader(sess)
 	file, err := req.File.Open()
-	
+
 	if err != nil {
 		fmt.Printf("Failed to open file: %v", err)
 		return
 	}
 	defer file.Close()
 
-	location, err := s3aws.UploadFile(uploader, file, bucketName,util.RandomString(20))
+	location, err := s3aws.UploadFile(uploader, file, bucketName, util.RandomString(20))
 	if err != nil {
 		fmt.Printf("Failed to upload file: %v", err)
 	}
-	fmt.Printf("file added in location %s",location)
+	fmt.Printf("file added in location %s", location)
 
 	//get urls
 	//add rabbitmq queue
 	// ProductUrls:=[]string{"https://firebasestorage.googleapis.com/v0/b/personal-website-cc143.appspot.com/o/B612_20241011_182211_278.jpg?alt=media&token=7f8c8632-881a-4585-8996-e93927758907"}
-	arg:= db.CreateProductParams{
-		ID               :req.ID,
-	UserID             :req.UserID ,
-	ProductName        :req.ProductName,
-	ProductDescription :req.ProductDescription,
-	ProductPrice      :req.ProductPrice ,
-	ProductUrls       :[]string{location},
+	arg := db.CreateProductParams{
+		ID:                 req.ID,
+		UserID:             req.UserID,
+		ProductName:        req.ProductName,
+		ProductDescription: req.ProductDescription,
+		ProductPrice:       req.ProductPrice,
+		ProductUrls:        []string{location},
 	}
 
-
-	
-	product , err:= server.store.CreateProduct(ctx,arg)
+	product, err := server.store.CreateProduct(ctx, arg)
 	if err != nil {
 		if db.ErrorCode(err) == db.UniqueViolation {
 			ctx.JSON(http.StatusForbidden, errorResponse(err))
@@ -89,23 +90,23 @@ func (server *Server) createProduct(ctx *gin.Context){
 	}
 
 	//signal rabbitmq
-	
+
 	payload := map[string]string{
-        "url":  location,
-        "product_id":req.ID, // Convert productID to string
-    }
-    jsonPayload, err := json.Marshal(payload)
-    if err != nil {
+		"url":        location,
+		"product_id": req.ID, // Convert productID to string
+	}
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
 		log.Fatal().Err(err).Msg("RabbitMQ marshal error")
 
-        // Handle error (e.g., retry, dead-letter queue)
-    }
+		// Handle error (e.g., retry, dead-letter queue)
+	}
 
 	err = server.rmqch.Publish(
-		"",     // exchange
+		"",            // exchange
 		server.q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
+		false,         // mandatory
+		false,         // immediate
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        jsonPayload,
@@ -115,57 +116,85 @@ func (server *Server) createProduct(ctx *gin.Context){
 
 		// Implement retry mechanism or error handling here
 	}
-
-	ctx.JSON(http.StatusOK,product)
-
 	
+	ctx.JSON(http.StatusOK, product)
 
 }
+
 type getProductByProductIDRequest struct {
-ID             string   `uri:"id"`
-
+	ID string `uri:"id"`
 }
-func (server *Server) getProductByProductID(ctx *gin.Context){
-	var req  getProductByProductIDRequest
-	// if err := ctx.ShouldBindJSON(&req); err != nil {
-	// 	ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	// 	return
-	// }
-	
-	
 
-	product , err:= server.store.GetProductByProductID(ctx,req.ID)
-	if err != nil {
-		if db.ErrorCode(err) == db.UniqueViolation {
-			ctx.JSON(http.StatusForbidden, errorResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+func (server *Server) getProductByProductID(ctx *gin.Context) {
+	var req getProductByProductIDRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK,product)
 
-	
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	val, err1 := client.Get(req.ID).Result()
+
+	var redisdata db.Products
+	err2 := json.Unmarshal([]byte(val), &redisdata)
+	if err1!=nil || err2!=nil {
+		fmt.Println(err1, err2)
+		product, err := server.store.GetProductByProductID(ctx, req.ID)
+		if err != nil {
+			if db.ErrorCode(err) == db.UniqueViolation {
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+			return
+		}
+		productmarshalled, err := json.Marshal(product)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	err = client.Set(product.ID, productmarshalled, 0).Err()
+	if err!=nil {
+		fmt.Print("error saving in redis")
+	}
+	fmt.Println("done caching in redis")
+		ctx.JSON(http.StatusOK, product)
+
+	} else {
+		fmt.Print("cached from redis")
+		ctx.JSON(http.StatusOK, redisdata)
+	}
 
 }
+
 type AddCompressedProductImageUrlsByIDRequest struct {
 	CompressedProductImagesUrls []string `json:"compressed_product_images_urls"`
 	ID                          string   `json:"id"`
 }
-func (server *Server) addCompressedImagesByProductID (ctx *gin.Context){
+
+func (server *Server) addCompressedImagesByProductID(ctx *gin.Context) {
 	var req AddCompressedProductImageUrlsByIDRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	arg:= db.AddCompressedProductImageUrlsByIDParams{
+	arg := db.AddCompressedProductImageUrlsByIDParams{
 		CompressedProductImagesUrls: req.CompressedProductImagesUrls,
-		ID: req.ID,
+		ID:                          req.ID,
 	}
-	product,err:= server.store.AddCompressedProductImageUrlsByID(ctx,arg)
-	if err!=nil {
+	product, err := server.store.AddCompressedProductImageUrlsByID(ctx, arg)
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK,product)
+	ctx.JSON(http.StatusOK, product)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/aws/aws-sdk-go v1.55.5
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/golang/mock v1.6.0
 	github.com/jackc/pgx/v5 v5.7.1
@@ -46,6 +47,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nxadm/tail v1.4.11 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
